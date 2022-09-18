7c05772ad8979d77895d7bda2d8dd34899c717bb
bomi <bonita9931@gmail.com>
2022-09-18 20:39:58 +0900

    feat: Junit4TestRunner 완료

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -8,6 +11,22 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
+        Junit4Test instance = clazz.getConstructor()
+                .newInstance();
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> invoke(instance, method));
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+    }
+
+    private void invoke(final Junit4Test instance, final Method method) {
+        try {
+            method.invoke(instance);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
