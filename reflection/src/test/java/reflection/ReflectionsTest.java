9d44fd17cf3ec47760fa25e5f99a38bd2c2b8708
bomi <bonita9931@gmail.com>
2022-09-18 22:50:27 +0900

    feat: ReflectionsTest 완료

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.HashSet;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -12,6 +16,13 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
+
+        final var classes = new HashSet<Class<?>>();
+        classes.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        classes.forEach(aClass -> log.info(aClass.getName()));
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
     }
