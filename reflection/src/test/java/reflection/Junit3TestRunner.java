3ba8ecc137bb455b8face70487aeeebb80309b72
bomi <bonita9931@gmail.com>
2022-09-18 20:39:51 +0900

    feat: Junit3TestRunner 완료

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -8,6 +11,22 @@ class Junit3TestRunner {
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
+        Junit3Test instance = clazz.getConstructor().newInstance();
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(x -> x.getName().startsWith("test"))
+                .forEach(method -> invoke(instance, method));
+
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+    }
+
+    private void invoke(final Junit3Test instance, final Method method) {
+        try {
+            method.invoke(instance);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
